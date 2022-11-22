9a3ee2c04205eac7b0fe0978fc89029a5499640a
TooColline <38856515+TooColline@users.noreply.github.com>
2022-11-23 00:06:01 +0300

    update whitelist

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 8080;
 corsProxy.createServer({
     originWhitelist: [
         'http://localhost:3000',
-        'https://weather-app-git-dev-collinstooct-gmailcom.vercel.app',
+        'https://too-collins-weather-app.herokuapp.com/',
+        'http://weather-app-git-dev-collinstooct-gmailcom.vercel.app',
         'https://api.openweathermap.org/data/2.5',
     ],
     requireHeader: ['origin', 'x-requested-with'],
