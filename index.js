cc6e89f945a72e74237b891cd1b520f52a52e237
TooColline <38856515+TooColline@users.noreply.github.com>
2022-11-22 22:43:25 +0300

    add packages and origin whitelist

---
+++ index.js
@@ -0,0 +1,17 @@
+const corsProxy = require('cors-anywhere');
+
+// Listen on a specific host via the HOST environment variable.
+const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable.
+const port = process.env.PORT || 8080;
+
+corsProxy.createServer({
+    originWhitelist: [
+        'http://localhost:3000',
+        'https://api.openweathermap.org/data/2.5',
+    ],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, () => {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
