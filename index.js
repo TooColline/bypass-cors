998d35fe72d38b06d9693be3382004c323d8e4be
TooColline <38856515+TooColline@users.noreply.github.com>
2022-11-23 19:57:44 +0300

    add procfile for heroku deployment

--- index.js
+++ index.js
@@ -8,16 +8,10 @@ const port = process.env.PORT || 8080;
 corsProxy.createServer({
     originWhitelist: [
         'http://localhost:3000',
-        'https://too-collins-weather-app.herokuapp.com/',
-        'https://weather-app-git-dev-collinstooct-gmailcom.vercel.app/',
-        'https://bypass-cors-two.vercel.app/',
-        'https://bypass-cors-git-main-collinstooct-gmailcom.vercel.app',
-        'https://bypass-cors-collinstooct-gmailcom.vercel.app'
+        'https://too-collins-weather-app.herokuapp.com/'
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
-
-module.exports = corsProxy
